12e1e0dd0f5260f75fb65a7783f9ee14546642b5
AhmadHasbyBik <abik262002@gmail.com>
2024-02-14 13:17:37 +0700

    add crud user

--- api.py
+++ api.py
@@ -1,12 +1,29 @@
 from flask import Flask, request, jsonify
+import pymongo
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 import pickle
+import bcrypt
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 class_names = ["Cyst", "Normal", "Stone", "Tumor"]
+
+
+# Konfigurasi MongoDB Atlas
+MONGO_URI = "mongodb+srv://abik26:abik12345@kidneydisease.lgwqxzc.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(MONGO_URI)
+db = client.get_database("kidneydisease")  # Ganti "your_database_name" dengan nama basis data Anda
+users = db.users
+
+# Cek koneksi MongoDB
+try:
+    client.server_info()
+    print("Connected to MongoDB Atlas")
+except pymongo.errors.ServerSelectionTimeoutError as e:
+    print("Failed to connect to MongoDB Atlas:", e)
 
 # Define the image size
 IMAGE_SIZE = 128
@@ -117,5 +134,96 @@ def predict():
     except Exception as e:
         return jsonify({'error': f'An error occurred: {str(e)}'})
 
+@app.route('/register', methods=['POST'])
+def register():
+    try:
+        # Get user input data
+        username = request.json['username']
+        fullname = request.json['fullname']
+        email = request.json['email']
+        password = request.json['password']
+        address = request.json['address']
+
+        # Check if username or email already exists
+        existing_user = users.find_one({'$or': [{'username': username}, {'email': email}]})
+
+        if existing_user:
+            return jsonify({'error': 'Username or email already exists!'})
+
+        # Hash password before saving to database
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+        # Insert user data into the database
+         # Insert user data into the database
+        result = users.insert_one({'username': username, 'fullname': fullname, 'email': email, 'password': hashed_password, 'address': address})
+
+        # Construct the response with newly created user data
+        new_user_data = {
+            'id': str(result.inserted_id),
+            'username': username,
+            'fullname': fullname,
+            'email': email,
+            'address': address
+        }
+
+        return jsonify({'message': 'User registered successfully!', 'user_data': new_user_data})
+    except Exception as e:
+        return jsonify({'error': f'An error occurred: {str(e)}'})
+
+# Fungsi Login Pengguna
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        login_user = users.find_one({'username': request.json['username']})
+
+        if login_user:
+            # Check if password matches hashed password in database
+            if bcrypt.checkpw(request.json['password'].encode('utf-8'), login_user['password']):
+                return jsonify({'message': 'Login successful!'})
+            else:
+                return jsonify({'error': 'Invalid username/password combination'})
+        else:
+            return jsonify({'error': 'User not found'})
+    except Exception as e:
+        return jsonify({'error': f'An error occurred: {str(e)}'})
+
+
+@app.route('/user/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    try:
+        # Convert the user_id to ObjectId
+        obj_id = ObjectId(user_id)
+
+        # Check if the user with the specified ID exists
+        user = users.find_one({'_id': obj_id})
+        if user is None:
+            return jsonify({'error': 'User not found!'})
+
+        # Delete the user
+        result = users.delete_one({'_id': obj_id})
+
+        if result.deleted_count == 1:
+            return jsonify({'message': 'User deleted successfully!'})
+        else:
+            return jsonify({'error': 'Failed to delete user!'})
+
+    except Exception as e:
+        return jsonify({'error': f'An error occurred: {str(e)}'})
+    
+@app.route('/user', methods=['GET'])
+def get_all_users():
+    try:
+        # Get all users from the database
+        all_users = list(users.find({}, {'password': 0}))
+
+        # Remove the 'password' field from each user (for security reasons)
+        for user in all_users:
+            user.pop('password', None)
+            user['_id'] = str(user['_id'])  # Convert ObjectId to string
+
+        return jsonify({'users': all_users})
+    except Exception as e:
+        return jsonify({'error': f'An error occurred: {str(e)}'})
+    
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+# Cek koneksi MongoDB
+# try:
+#     mongo.db.command('ping')
+#     print("Connected to MongoDB Atlas")
+# except pymongo.errors.ServerSelectionTimeoutError as e:
+#     print("Failed to connect to MongoDB Atlas:", e)
